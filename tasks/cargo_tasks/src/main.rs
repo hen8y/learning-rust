f91d794b8aa5b6e908196f1d7626a9d8a0d75d92
hen8y <dheryhenry@gmail.com>
2024-07-06 02:34:24 +0100

    Task 7

--- tasks/cargo_tasks/src/main.rs
+++ tasks/cargo_tasks/src/main.rs
@@ -1,7 +1,9 @@
 // mod random_num_to_function_5;
-mod guessing_game_6;
+// mod guessing_game_6;
+ mod numeral_reversal;
 
 fn main() {
     // random_num_to_function_5::random_func();
-    guessing_game_6::guess();
+    // guessing_game_6::guess();
+    numeral_reversal::reversal();
 }
---
+++ tasks/cargo_tasks/src/numeral_reversal.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn reversed_string(number: u64) -> String {
+    let result = number.to_string().chars().rev().collect();
+    result
+}
+
+
+pub fn reversal() {
+    println!("Enter a number");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read input");
+
+    let number = input.trim().parse::<u64>().expect("input not a number");
+
+    println!("Your reserved number: {}", reversed_string(number));
+}
